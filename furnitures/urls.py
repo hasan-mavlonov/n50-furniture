1291cd2c74846562172a221b5896a2a310ac7655
Hasan Mavlonov <hmavlanov79@gmail.com>
2024-10-06 17:24:20 +0500

    cahanged

--- furnitures/urls.py
+++ furnitures/urls.py
@@ -5,7 +5,7 @@ from users.views import login_page, register_page, user_page
 
 urlpatterns = [
     path('', home_page_view, name='homepage'),
-    path('/login/', login_page, name='login'),
-    path('/register/', register_page, name='register'),
-    path('/user/', user_page, name='user'),
+    path('login/', login_page, name='login'),
+    path('register/', register_page, name='register'),
+    path('user/', user_page, name='user'),
 ]
